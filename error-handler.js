7b3019addea94e0eb8287f0818c556050d916dad
Loker Kendari Official <lokerkendariofficial@gmail.com>
2026-06-20 23:18:29 +0800

    Create error-handler.js

---
+++ error-handler.js
@@ -0,0 +1,10 @@
+// ===== ERROR HANDLER - FILE INDUK =====
+// Import semua modul error dari folder js/error/
+
+import './js/error/global-error.js';
+import './js/error/console-override.js';
+import './js/error/notification.js';
+import './js/error/file-checker.js';
+import './js/error/system-info.js';
+
+console.log('✅ Error handler siap!');
