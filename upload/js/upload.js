3ea6ca66e17bafd15e1b18577cfa1e216ddc7a56
Loker Kendari Official <lokerkendariofficial@gmail.com>
2026-06-21 14:22:34 +0800

    Delete upload.js

--- upload/js/upload.js
+++
@@ -1,7 +0,0 @@
-// ============================================================
-// UPLOAD.JS - FILE INDUK (Entry Point)
-// ============================================================
-
-import './js/upload-main.js';
-
-console.log('🚀 Upload module loaded!');
