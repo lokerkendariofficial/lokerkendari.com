c92377138ba1cd61b6ff642b47779e01e9ddb5f6
Loker Kendari Official <lokerkendariofficial@gmail.com>
2026-06-21 21:50:55 +0800

    Update error-handler.js

--- js/core/error-handler.js
+++ js/core/error-handler.js
@@ -3,10 +3,11 @@
 // ============================================================
 // Mengimpor semua modul error dari folder js/error/
 
-import './js/error/global-error.js';
-import './js/error/console-override.js';
-import './js/error/notification.js';
-import './js/error/file-checker.js';
-import './js/error/system-info.js';
+// ✅ Path yang benar: naik satu folder (../) dari js/core/ ke js/
+import '../error/global-error.js';
+import '../error/console-override.js';
+import '../error/notification.js';
+import '../error/file-checker.js';
+import '../error/system-info.js';
 
 console.log('✅ Error handler siap!');
